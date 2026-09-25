c172e722cdf1af3ba33c4113de2ddcf673bba858
nithin-aashik-mcw <nithin.aashik@multicorewareinc.com>
2026-09-25 03:40:37 -0700

    test

--- BUILDBINARIES_EXAMPLE.bat
+++ BUILDBINARIES_EXAMPLE.bat
@@ -9,17 +9,13 @@
 :: You should write yourself a script that copies the files where you want them.
 :: Also see: https://github.com/assimp/assimp/pull/2646
 
-:: Detect the host architecture
+SET SOURCE_DIR=.
+SET GENERATOR=Visual Studio 17 2022
 SET ARCH=%PROCESSOR_ARCHITECTURE%
 IF /I "%ARCH%"=="ARM64" SET ARCH=ARM64
 IF /I "%ARCH%"=="AMD64" SET ARCH=x64
 IF /I "%ARCH%"=="x86" SET ARCH=Win32
 
-SET SOURCE_DIR=.
-SET GENERATOR=Visual Studio 17 2022
-
-:: ARM64 needs the "MSVC ARM64 build tools" component of Visual Studio.
-:: The assimp viewer is not built for ARM64 because the legacy D3DX9 library is x86/x64 only.
 SET BINARIES_DIR="./build/%ARCH%"
 cmake . -G "%GENERATOR%" -A %ARCH% -S %SOURCE_DIR% -B %BINARIES_DIR%
 cmake --build %BINARIES_DIR% --config debug
