8feff5e539dd9c0414af69a973419541126c7f7a
nithin-aashik-mcw <nithin.aashik@multicorewareinc.com>
2026-09-24 22:23:42 -0700

    CI Test

--- BUILDBINARIES_EXAMPLE.bat
+++ BUILDBINARIES_EXAMPLE.bat
@@ -10,7 +10,7 @@
 :: Also see: https://github.com/assimp/assimp/pull/2646
 
 SET SOURCE_DIR=.
-SET GENERATOR=Visual Studio 16 2019
+SET GENERATOR=Visual Studio 17 2022
 
 SET BINARIES_DIR="./build/Win32"
 cmake . -G "%GENERATOR%" -A Win32 -S %SOURCE_DIR% -B %BINARIES_DIR%
@@ -19,6 +19,13 @@ cmake --build %BINARIES_DIR% --config release
 
 SET BINARIES_DIR="./build/x64"
 cmake . -G "%GENERATOR%" -A x64 -S %SOURCE_DIR% -B %BINARIES_DIR%
+cmake --build %BINARIES_DIR% --config debug
+cmake --build %BINARIES_DIR% --config release
+
+:: ARM64 needs the "MSVC ARM64 build tools" component of Visual Studio.
+:: The assimp viewer is not built for ARM64 because the legacy D3DX9 library is x86/x64 only.
+SET BINARIES_DIR="./build/ARM64"
+cmake . -G "%GENERATOR%" -A ARM64 -S %SOURCE_DIR% -B %BINARIES_DIR%
 cmake --build %BINARIES_DIR% --config debug
 cmake --build %BINARIES_DIR% --config release
 
