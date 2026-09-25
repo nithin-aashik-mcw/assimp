d7ce3cfda02e7d3862a9413f22f6d1a56e080b21
nithin-aashik-mcw <nithin.aashik@multicorewareinc.com>
2026-09-25 03:37:05 -0700

    test

--- BUILDBINARIES_EXAMPLE.bat
+++ BUILDBINARIES_EXAMPLE.bat
@@ -12,20 +12,16 @@
 SET SOURCE_DIR=.
 SET GENERATOR=Visual Studio 17 2022
 
-SET BINARIES_DIR="./build/Win32"
-cmake . -G "%GENERATOR%" -A Win32 -S %SOURCE_DIR% -B %BINARIES_DIR%
-cmake --build %BINARIES_DIR% --config debug
-cmake --build %BINARIES_DIR% --config release
-
-SET BINARIES_DIR="./build/x64"
-cmake . -G "%GENERATOR%" -A x64 -S %SOURCE_DIR% -B %BINARIES_DIR%
-cmake --build %BINARIES_DIR% --config debug
-cmake --build %BINARIES_DIR% --config release
+:: Detect the host architecture
+SET ARCH=%PROCESSOR_ARCHITECTURE%
+IF /I "%ARCH%"=="ARM64" SET ARCH=ARM64
+IF /I "%ARCH%"=="AMD64" SET ARCH=x64
+IF /I "%ARCH%"=="x86" SET ARCH=Win32
 
 :: ARM64 needs the "MSVC ARM64 build tools" component of Visual Studio.
 :: The assimp viewer is not built for ARM64 because the legacy D3DX9 library is x86/x64 only.
-SET BINARIES_DIR="./build/ARM64"
-cmake . -G "%GENERATOR%" -A ARM64 -S %SOURCE_DIR% -B %BINARIES_DIR%
+SET BINARIES_DIR="./build/%ARCH%"
+cmake . -G "%GENERATOR%" -A %ARCH% -S %SOURCE_DIR% -B %BINARIES_DIR%
 cmake --build %BINARIES_DIR% --config debug
 cmake --build %BINARIES_DIR% --config release
 
